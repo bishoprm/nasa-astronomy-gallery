2d74f73692c1ef65043b060e53266eb701530b28
bishoprm <bishop.rose@gmail.com>
2020-08-17 17:23:21 +0200

    :art: filtered photo results from one api call

--- src/App.js
+++ src/App.js
@@ -3,25 +3,54 @@ import "./App.scss";
 import { getNASAPictures } from "./NasaAPI";
 import ImageCard from "./components/ImageCard";
 import SelectDate from "./components/SelectDate";
+import Loading from "./components/Loading";
 import moment from "moment-timezone";
 
 function App() {
-  const [pictures, updatePictures] = useState(null);
-  const [date, setDate] = useState(20);
+  const [pictures, updatePictures] = useState([]);
+  const [dateFilter, setDateFilter] = useState("lastMonth");
 
   // the API is set in EST so I used momentjs instead of the getDate method.
   // Otherwise it will break if a user elsewhere uses it after midnight
   useEffect(() => {
-    const startDate = moment.tz("America/New_York").subtract("days", date);
+    const startDate = moment.tz("America/New_York").subtract("days", 29);
     const endDate = moment.tz("America/New_York");
     getNASAPictures(startDate, endDate).then((res) => {
       updatePictures(res);
     });
-  }, [date]);
+  }, []);
 
-  const handleChangeDate = (event) => {
-    setDate(event.target.value);
-  };
+  function dateFilterHandler(event) {
+    setDateFilter(event.target.value);
+  }
+
+  const filterPhotos = pictures.filter((picture, index) => {
+    // the amount of photos shown might not equal one per day,
+    // since only photos are rendered in this app and sometimes NASA posts videos
+    if (dateFilter === "lastMonth") {
+      return picture;
+    } else if (dateFilter === "lastTwoWeeks" && index >= 14) {
+      return picture;
+    } else if (dateFilter === "lastWeek" && index >= 23) {
+      return picture;
+    }
+  });
+
+  function displayPhotos() {
+    if (pictures.length === 0) {
+      return <Loading />;
+    } else {
+      return (
+        <div className="container">
+          {filterPhotos.map((picture) => (
+            <div key={picture.date}>
+              {picture.media_type === "image" && <ImageCard {...picture} />}
+            </div>
+          ))}
+        </div>
+      );
+    }
+  }
 
   return (
     <div>
@@ -32,15 +61,8 @@ function App() {
           most recent photos!
         </p>
       </header>
-      <SelectDate handleChangeDate={handleChangeDate} />
-      <div className="container">
-        {pictures &&
-          pictures.map((picture) => (
-            <div key={picture.date}>
-              {picture.media_type === "image" && <ImageCard {...picture} />}
-            </div>
-          ))}
-      </div>
+      <SelectDate dateFilterHandler={dateFilterHandler} />
+      {pictures && displayPhotos()}
     </div>
   );
 }
---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div>
+      <h2>
+        <span role="img" aria-label="earth">
+          🌍
+        </span>
+      </h2>
+    </div>
+  );
+}
